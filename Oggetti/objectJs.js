395124e1c555caeffa0f3a36b5e280d0e8ffd107
ValerioCaporali <valerio.caporali@divini.org>
2019-03-09 09:37:32 +0100

    h
    oggetti

---
+++ Oggetti/objectJs.js
@@ -0,0 +1,40 @@
+//es 1
+var string = new Object();
+
+string[0] = 'a';
+string[1] = 'b';
+string[2] = 'c';
+string[3] = 'd';
+string[4] = 'e';
+string[5] = 'f';
+
+//es 2
+var string1 = new String("abcdef");
+
+//es 3
+console.log(string);
+console.log(string1);
+
+
+// es 4
+var f = function (a,b)
+
+var c;
+var d;
+
+var oggetto = new f(c,d);
+
+//es 5
+
+var z = new Number();
+console.log(z.constructor)
+
+var y = new String ();
+console.log(y.constructor)
+
+var x = new Boolean();
+console.log(x.constructor)
+
+var z = new Object();
+console.log(z.constructor)
+
